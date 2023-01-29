85325c73c5d184db70c799e0abfae3009bcbb616
João Amaral <amaral.joao@ua.pt>
2023-01-29 22:41:59 +0000

    section 11 cont...

--- Angular/10 - Routing/baseProject/src/app/app.module.ts
+++ Angular/10 - Routing/baseProject/src/app/app.module.ts
@@ -11,6 +11,19 @@ import { UserComponent } from './users/user/user.component';
 import { EditServerComponent } from './servers/edit-server/edit-server.component';
 import { ServerComponent } from './servers/server/server.component';
 import { ServersService } from './servers/servers.service';
+import { RouterModule, Routes } from '@angular/router';
+
+const appRoutes: Routes = [
+  { path: "", component: HomeComponent },
+  {//Tem seguir sempre a seguinte estrutura
+    path: "users", //sem o char '/'
+    component: UsersComponent
+  },
+  { path: "users/:id/:name", component: UserComponent},
+  { path: "servers", component: ServersComponent},
+
+]
+
 
 @NgModule({
   declarations: [
@@ -25,6 +38,7 @@ import { ServersService } from './servers/servers.service';
   imports: [
     BrowserModule,
     FormsModule,
+    RouterModule.forRoot(appRoutes) //Permite registar a rotas para a aplicação base
   ],
   providers: [ServersService],
   bootstrap: [AppComponent]
