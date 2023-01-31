292a0375a348dcb6652de4c1179d7e573bf9920a
TMnd <amaral.joao@ua.pt>
2023-01-31 20:59:16 +0000

    section 11 before authGuard

--- Angular/10 - Routing/baseProject/src/app/app.module.ts
+++ Angular/10 - Routing/baseProject/src/app/app.module.ts
@@ -11,19 +11,8 @@ import { UserComponent } from './users/user/user.component';
 import { EditServerComponent } from './servers/edit-server/edit-server.component';
 import { ServerComponent } from './servers/server/server.component';
 import { ServersService } from './servers/servers.service';
-import { RouterModule, Routes } from '@angular/router';
-
-const appRoutes: Routes = [
-  { path: "", component: HomeComponent },
-  {//Tem seguir sempre a seguinte estrutura
-    path: "users", //sem o char '/'
-    component: UsersComponent
-  },
-  { path: "users/:id/:name", component: UserComponent},
-  { path: "servers", component: ServersComponent},
-
-]
-
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
@@ -33,12 +22,13 @@ const appRoutes: Routes = [
     ServersComponent,
     UserComponent,
     EditServerComponent,
-    ServerComponent
+    ServerComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    RouterModule.forRoot(appRoutes) //Permite registar a rotas para a aplicação base
+    AppRoutingModule
   ],
   providers: [ServersService],
   bootstrap: [AppComponent]
