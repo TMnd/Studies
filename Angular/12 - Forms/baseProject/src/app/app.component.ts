6bcb5d3a8cec49a2533a10f7f6dc24b445a00a92
João Amaral <amaral.joao@ua.pt>
2023-02-20 22:20:59 +0000

    Section 15 - Template driven form - completed

--- Angular/12 - Forms/baseProject/src/app/app.component.ts
+++ Angular/12 - Forms/baseProject/src/app/app.component.ts
@@ -9,12 +9,33 @@ import { NgForm } from '@angular/forms';
 export class AppComponent {
   @ViewChild('f') signupForm!: NgForm;
   defaultQuestion = 'pet';
-
   anwser: string = '';
-
+  genders = ['male','female'];
+  user = {
+    username: '',
+    email: '',
+    secretQuestion: '',
+    answer: '',
+    gender: ''
+  };
+  submitted = false;
 
   suggestUserName() {
     const suggestedName = 'Superuser';
+    /* this.signupForm.setValue({
+      userData: {
+        username: suggestedName,
+        email: '',
+      },
+      secret: 'pet',
+      questionAnswer: '',
+      gender: 'male'
+    }); */
+    this.signupForm.form.patchValue({
+      userData: {
+        username: suggestedName
+      }
+    });
   }
 
   // onSubmit(data: NgForm) {
@@ -22,6 +43,13 @@ export class AppComponent {
   // }
 
   onSubmit(data: NgForm) {
-    console.log(this.signupForm);
+    this.submitted = true;
+    this.user.username = this.signupForm.value.userData.username;
+    this.user.email = this.signupForm.value.userData.email;
+    this.user.secretQuestion = this.signupForm.value.secret;
+    this.user.answer = this.signupForm.value.questionAnswer;
+    this.user.gender = this.signupForm.value.gender;
+
+    this.signupForm.reset();
   }
 }
