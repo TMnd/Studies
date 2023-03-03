b20e28abe9210a3af7d615b1c3dd2a961a59e7f5
João Amaral <amaral.joao@ua.pt>
2023-03-03 23:15:05 +0000

    Section 15 - 215

--- Angular/12 - Forms/reactiveApproachFormProject/src/app/app.component.ts
+++ Angular/12 - Forms/reactiveApproachFormProject/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -19,11 +20,39 @@ export class AppComponent implements OnInit{
     this.signupForm = new FormGroup({
       'userData': new FormGroup({
         'username': new FormControl(null, [Validators.required, this.forbiddenNames.bind(this)]),
-        'email': new FormControl(null, [Validators.required, Validators.email]),
+        'email': new FormControl(null, [Validators.required, Validators.email], this.forbiddenEmails),
       }),
       'gender': new FormControl('male'),
       'hobbies': new FormArray([]) 
     });
+    /* this.signupForm.valueChanges.subscribe(
+      (value) => {
+        console.log(value);
+      }
+    ); */
+    this.signupForm.statusChanges.subscribe(
+      (value) => {
+        console.log(value);
+      }
+    );
+
+    /* this.signupForm.patchValue({
+      'userData':{
+        'username': 'Anna',
+        'email':'anna@test.com'
+      },
+      'gender': 'female',
+      'hobbies': []
+    }) */
+
+    this.signupForm.setValue({
+      'userData':{
+        'username': 'Max',
+        'email':'max@test.com'
+      },
+      'gender': 'male',
+      'hobbies': []
+    })
   }
 
   forbiddenNames(control: FormControl): {[s: string]: boolean} {
@@ -35,6 +64,7 @@ export class AppComponent implements OnInit{
 
   onSubmit() {
     console.log(this.signupForm);
+    this.signupForm.reset(); //To reset the form
   }
 
   onAddHobby() {
@@ -44,5 +74,18 @@ export class AppComponent implements OnInit{
 
   getControls() {
     return (<FormArray>this.signupForm.get('hobbies')).controls;
+  }
+
+  forbiddenEmails(control: FormControl): Promise<any> | Observable<any> {
+    const promise = new Promise<any>((resolve, reject) => {
+      setTimeout(() => {
+        if (control.value === 'test@test.com'){
+          resolve({'emailIsForbidden': true});
+        } else {
+          resolve(null);
+        }
+      }, 1500);
+    });
+    return promise;
   }
 }
