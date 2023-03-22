54dda98a366f2626839b5f6a69dd83d3fd8a6e86
TMnd <amaral.joao@ua.pt>
2023-03-22 23:14:18 +0000

    Fix merge problems from last commit

--- Angular/14 - Http Request/baseProject/src/app/app.component.ts
+++ Angular/14 - Http Request/baseProject/src/app/app.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/operators';
+import { Post } from './post.mode';
+import { PostService } from './posts.service';
 
 @Component({
   selector: 'app-root',
@@ -8,21 +11,42 @@ import { HttpClient } from '@angular/common/http';
 })
 export class AppComponent implements OnInit {
   loadedPosts = [];
+  isFetchingPosts = false;
 
-  constructor(private http: HttpClient) {}
+  constructor(
+    private http: HttpClient, 
+    private postsService: PostService
+  ) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.postsService.fetchPosts()
+    .subscribe(postsArray => {
+        this.isFetchingPosts = false;
+        this.loadedPosts = postsArray;
+    });;
+  }
 
   onCreatePost(postData: { title: string; content: string }) {
     // Send Http request
-    console.log(postData);
+    this.postsService.createAndStorePost(postData.title, postData.content);
   }
 
   onFetchPosts() {
     // Send Http request
+    this.isFetchingPosts = true;
+    this.postsService.fetchPosts()
+    .subscribe(postsArray => {
+        this.isFetchingPosts = false;
+        this.loadedPosts = postsArray;
+    });
   }
 
   onClearPosts() {
     // Send Http request
+    this.isFetchingPosts = true;
+    this.postsService.deletePosts().subscribe(() => {
+      this.isFetchingPosts = false;
+      this.loadedPosts = [];
+    });
   }
 }
