32362f717556dadc7a6b25c01ae7e95d353e66f4
João Amaral <amaral.joao@ua.pt>
2023-05-27 00:17:04 +0100

    Section 27 - 438 (Realone, the last commit was 432)

--- Angular/18 - Animations/src/app/app.component.ts
+++ Angular/18 - Animations/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { animate, state, style, transition, trigger } from '@angular/animations';
+import { animate, keyframes, state, style, transition, trigger, group } from '@angular/animations';
 import { Component } from '@angular/core';
 
 @Component({
@@ -32,8 +32,73 @@ import { Component } from '@angular/core';
       })),
       transition('normal => highlighted', animate(300)),
       transition('highlighted => normal', animate(800)),
-      transition('shrunken <=> *', animate(500)),
-    ])
+      transition('shrunken <=> *', [
+        style({
+          backgroundColor: 'orange'
+        }),
+        animate(1000, style({
+          borderRadius: '50px'
+        })),
+        animate(500)
+      ])
+    ]),
+    trigger('list1', [
+      state('in', style({
+        opacity: 1,
+        transform: 'translateX(0)'
+      })),
+      transition('void <=> *', [style({
+        opacity: 0,
+        transform: 'translateX(-100px)'
+      })]),
+      animate(300),
+      transition('void <=> *', [
+        animate(300), style({
+          transform: 'translateLateX(100px)'
+        })
+      ]),
+    ]),
+    trigger('list1', [
+      state('in', style({
+        opacity: 1,
+        transform: 'translateX(0)'
+      })),
+      transition('void <=> *', [
+        animate(1000,keyframes([
+          style({
+            transform: 'translateX(-100px)*',
+            opacity: 0,
+            offset: 0
+          }),
+          style({
+            transform: 'translateX(-50px)',
+            opacity: 0.5,
+            offset: 0.3
+          }),
+          style({
+            transform: 'translateX(-20px)',
+            opacity: 1,
+            offset: 0.8
+          }),
+          style({
+            transform: 'translateX(0px)',
+            opacity: 1,
+            offset: 1
+          })
+        ]))
+      ]),
+      transition('void <=> *', [
+        group([
+          animate(300), style({
+            color: ''
+          }),
+          animate(300), style({
+            transform: 'translateX(100px)',
+            opacity: 0.5
+          })
+        ])
+      ]),
+    ]),
   ]
 })
 export class AppComponent {
@@ -54,5 +119,13 @@ export class AppComponent {
 
   onAdd(item) {
     this.list.push(item);
+  }
+
+  animationStarted(event) {
+    console.log(event)
+  }
+
+  animationEnded(event) {
+    console.log(event)
   }
 }
